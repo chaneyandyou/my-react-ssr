05a533091f3ab2d5908cbf5de097713cbd2f875e
chaneyandyou <chaneychan@126.com>
2019-02-07 09:46:12 +0800

    feat:basic ssr

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import React from 'react'
+import { renderToString } from 'react-dom/server'
+import Home from './containers/Home/index'
+const app = express()
+const content = renderToString(<Home />)
+
+app.get('/', (req, res) => {
+  res.send(`
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <title>react ssr</title>
+    </head>
+    <body>
+        ${content}
+    </body>
+    </html>
+  `)
+})
+
+const server = app.listen(3000);
---
+++ webpack.server.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const nodeExternals = require('webpack-node-externals')
+
+module.exports = {
+  target: 'node',
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build')
+  },
+  externals: [nodeExternals()],
+
+  module: {
+    rules: [{
+      test: /\.js?$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/,
+      options: {
+        presets: ['react', 'stage-0', ['env', {
+          target: {
+            browsers: ['last 2 versions']
+          }
+        }]]
+      }
+    }]
+  }
+}
