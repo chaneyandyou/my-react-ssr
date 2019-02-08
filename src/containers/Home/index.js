d48e3250a3be79571608874826f22791bde4a306
chaneyandyou <chaneychan@126.com>
2019-02-08 08:39:23 +0800

    feat:使客户端同样执行一次前端代码进行同构；整理webpack打包配置

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,7 +1,12 @@
 import React from 'react'
 
 const Home = () => {
-  return <div>Home component, this is chaney</div>
+  return (
+    <div>
+      Home component, this is chaney
+      <button onClick={() => { alert('click') }}>click</button>
+    </div>
+  )
 }
 
 export default Home
--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,11 @@
 import express from 'express'
 import React from 'react'
 import { renderToString } from 'react-dom/server'
-import Home from './containers/Home/index'
+import Home from '../containers/Home/index'
 const app = express()
 const content = renderToString(<Home />)
+
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.send(`
@@ -16,7 +18,8 @@ app.get('/', (req, res) => {
       <title>react ssr</title>
     </head>
     <body>
-        ${content}
+        <div id="root">${content}</div>
+        <script src='/index.js'></script
     </body>
     </html>
   `)
