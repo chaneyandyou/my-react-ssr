d10ca340f0ad2c55202433e6a12c36c258e38522
chaneyandyou <chaneychan@126.com>
2019-02-14 23:40:57 +0800

    feat:使用redux-thunk的withExtraArgument方法区分服务端与客户端的request

--- src/containers/Home/store/action.js
+++ src/containers/Home/store/action.js
@@ -1,19 +1,14 @@
 import axios from 'axios'
 import { CHANGE_LIST } from './constant'
-import clientRequest from '../../../client/request'
-import serverRequest from '../../../server/request'
 
 const changeList = (list) => ({
   type: CHANGE_LIST,
   list
 })
 
-export const getHomeList = (server) => {
-
-  const request = server ? serverRequest : clientRequest
-
-  return (dispatch) => {
-    return request.get('/api/v1/topics')
+export const getHomeList = () => {
+  return (dispatch, getState, axiosInstance) => {
+    return axiosInstance.get('/api/v1/topics')
     .then(res => {
       const list = res.data.data
       dispatch(changeList(list))
--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,24 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux'
 import thunk from 'redux-thunk'
 import { reducer as homeReducer } from '../containers/Home/store'
+import serverAxios from '../server/request'
+import clientAxios from '../client/request'
 
 const reducer = combineReducers({
   home: homeReducer
 })
 
+/**
+ * server服务端store
+ */
 export const getStore = () => {
-  return createStore(reducer, applyMiddleware(thunk))
+  return createStore(reducer, applyMiddleware(thunk.withExtraArgument(serverAxios)))
 }
 
+/**
+ * client客户端store
+ */
 export const getClientStore = () => {
   const defaultStore = window.context.state
-  return createStore(reducer, defaultStore, applyMiddleware(thunk))
+  return createStore(reducer, defaultStore, applyMiddleware(thunk.withExtraArgument(clientAxios)))
 }
